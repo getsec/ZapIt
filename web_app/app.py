58a7dee43267ab68f781f3c8dfcd3909ed267095
Nathan Getty <ngetty@wawanesa.com>
2019-05-02 09:39:06 -0500

    added feature for scan progress and results downloaded

--- web_app/app.py
+++ web_app/app.py
@@ -1,34 +1,58 @@
 import flask
 import requests
-import urllib.parse as encode_url
-from flask import request, jsonify, abort, render_template
+from flask import request, abort, render_template
 
 app = flask.Flask(__name__)
 
 # ZAP INFORMATION
-ZAP_URL = 'http://10.100.92.156'
-ZAP_PORT = '1337'
+ZAP_URL = 'http://10.100.92.156'  # TODO: replace with environment variable
+ZAP_PORT = '1337'   # TODO: replace with environment variable
 ZAP_SPIDER_SCAN = '/JSON/spider/action/scan'
-
+ZAP_SPIDER_STATUS = '/JSON/spider/view/status'
+ZAP_SPIDER_RESULTS = '/JSON/spider/view/results'
 
 zap_scan_spider_uri = f"{ZAP_URL}:{ZAP_PORT}{ZAP_SPIDER_SCAN}"
+zap_scan_spider_status = f"{ZAP_URL}:{ZAP_PORT}{ZAP_SPIDER_STATUS}"
+zap_scan_spider_results = f"{ZAP_URL}:{ZAP_PORT}{ZAP_SPIDER_RESULTS}"
 
 
 def post_scan_start(zap_scan_spider_uri, requested_url):
-    encoded_url = encode_url.quote(requested_url)
     post_data = {
         'zapapiformat': 'JSON',
         'formMethod': 'POST',
-        'url': encoded_url,
-        'maxChildren': 1,
-        'recurse': 'False',
+        'url': requested_url,
+        'maxChildren': '',
+        'recurse': 'True',
         'contextName': '',
-        'subtreeOnly': 'None'
+        'subtreeOnly': ''
     }
     data = requests.post(zap_scan_spider_uri, data=post_data)
     # print(post_data)
+    # print(post_data)
     # print(data.content)
     return data.content
+
+
+def post_scan_status(zap_scan_spider_status, scan_id):
+    post_data = {
+        'zapapiformat': 'JSON',
+        'formMethod': 'POST',
+        'scanId': scan_id
+    }
+    progress = requests.post(zap_scan_spider_status, data=post_data)
+    return progress.content
+
+
+def post_scan_results(zap_scan_spider_results, scan_id, format):
+    post_data = {
+        'zapapiformat': 'JSON',
+        'formMethod': format,
+        'scanId': scan_id
+    }
+    results = requests.post(zap_scan_spider_results, data=post_data)
+    print(post_data)
+    print(results.content)
+    return results.content
 
 
 @app.route("/", methods=["GET"])
@@ -37,13 +61,12 @@ def home():
 
 
 @app.route("/api/v1/scan/start", methods=["POST"])
-def scan_start():
-
+def spider_start():
     # Setting up some message for the response
     param = 'url'
     acceptance_strings = [
         'wmic.ins',
-        '.ins'
+        'wawanesa.com'
     ]
     resrict = "Resticted domain used in URL '{}'"
     example_json = "{\n  \"url\":\"https://xxx.com\"\n}"
@@ -55,16 +78,16 @@ def scan_start():
         # Ensure the url param was sent to the api
         if request.json['url']:
             requested_url = request.json['url']
-            print(f"URL = {requested_url}")
+
             # Ensure that the url is within the whitelist
-            for bad_string in acceptance_strings:
-                if bad_string in request.json['url']:
-                    scan_results = post_scan_start(zap_scan_spider_uri,
-                                                   requested_url)
-                    return scan_results
-                else:
-                    # if not return the error to the user
-                    return resrict.format(requested_url)
+            for match in acceptance_strings:
+                if requested_url.endswith(match):
+                    scan_id = post_scan_start(zap_scan_spider_uri,
+                                              requested_url)
+                    return scan_id
+            else:
+                # if not return the error to the user
+                return resrict.format(requested_url)
         else:
             return f"No {param} Parameter passed"
     except KeyError:
@@ -72,22 +95,42 @@ def scan_start():
 
 
 @app.route("/api/v1/scan/progress", methods=["POST"])
-def scan_progress():
+def spider_progress():
     param = 'id'
     example_json = "{\n  \"id\":\"4\"\n}"
     if not request.json:
         abort(400)
-    print(f"input: {request.json}")
     try:
         if request.json['id']:
-            output = {
-                param: request.json[param]
-            }
-            return jsonify(output)
+            scan_id = request.json['id']
+            scan_progress = post_scan_status(zap_scan_spider_status,
+                                             scan_id)
+            return scan_progress
         else:
             return f"No {param} Parameter passed"
     except KeyError:
         return f"Incorrect synax.\nmethod = POST\nexample \n{example_json}"
+
+
+@app.route("/api/v1/scan/results", methods=["POST"])
+def spider_results():
+    example_json = "{\n  \"format\":\"json\", \"id\":\"2\"\n}"
+    msg = f"missing id or format param in request\n\n{example_json}"
+    if not request.json:
+        abort(400)
+
+    try:
+        if request.json['id'] and request.json['format']:
+            scan_id = request.json['id']
+            format = request.json['format'].upper()
+            spider_results = post_scan_results(zap_scan_spider_results,
+                                               scan_id,
+                                               format)
+            return spider_results
+        else:
+            return msg
+    except KeyError:
+        return msg
 
 
 if __name__ == '__main__':
